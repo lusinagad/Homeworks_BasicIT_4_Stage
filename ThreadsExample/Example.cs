72010649a91ba60bb4ac0bee537ac1bcfd115e2e
lusinagad <agad.lusin@gmail.com>
2022-10-22 00:44:32 +0400

    Thread, Async/await example

---
+++ ThreadsExample/Example.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Example
+    {
+        public void FirstExmp()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("1 ");
+            }
+        }
+        public void SecondExmp()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("2 ");
+            }
+        } 
+       public async void firstExmp()
+        {
+
+            await Task.Delay(1000);
+                for (int i = 0; i < 100; i++)
+                { 
+                 Console.Write("1 "); 
+                
+                }            
+        }
+        public async void secondExmp()
+        {
+            await Task.Delay(1500);
+            for (int i =0;i<100;i++)
+            {
+                Console.Write("2 ");
+            }
+        }
+    }
+}
---
+++ ThreadsExample/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Example example = new Example();
+            example.FirstExmp();
+            example.SecondExmp();
+            Console.Read();
+
+            Thread firstThread = new Thread(example.FirstExmp);   // multithreading
+            Thread  secondThread = new Thread(example.SecondExmp);
+
+            firstThread.Start();
+            secondThread.Start();
+
+           
+            example.firstExmp(); // asynchronous 
+            example.secondExmp(); 
+
+            
+
+        }               
+    }
+}
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
